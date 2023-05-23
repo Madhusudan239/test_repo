079be0b35cf81b72a30204e18ae82c67300acf2c
Madhusudan239 <120104459+Madhusudan239@users.noreply.github.com>
2023-05-23 17:33:24 +0530

    Add files via upload

---
+++ test_pyfile.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+
+class test_testcases():
+    
+    def sumof_num(self, x):
+        x1 = x+x
+        
+        return x1
+    
+    def multiplication_num(self, y):
+        y1 = y*y
+        return y1
+    
+    def number_calculations(self,z):
+        sumof = self.sumof_num(z)
+        multiplication_of = self.multiplication_num(sumof)
+        
+        print(multiplication_of)
+    
+    
+    
+        
